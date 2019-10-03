4c196fe97d76c690dd9ff68d499286caa5962f77
Micky Hulse <mhulse@concentricsky.com>
2019-10-03 13:37:52 -0700

    Add dev.bash for #210

---
+++ .bash/dev.bash
@@ -0,0 +1,26 @@
+# Default Python:
+alias python=python3
+# Default pip:
+alias pip=pip3
+
+# Apache Tomcat start:
+# $ tomcat
+# $ tomcat start
+# Tomcat stop:
+# $ tomcat stop
+alias tomcat=catalina
+
+# List Atom packages:
+alias atompkgs="apm list --installed --bare"
+
+# Start blender from command line, used for debugging:
+alias blender="/Applications/blender.app/Contents/MacOS/blender"
+
+# Open current directory in Finder.app and Atom.app:
+alias go="open . && atom ."
+
+# XAMPP shortcuts:
+alias xampp="sudo /Applications/XAMPP/xamppfiles/xampp restart"
+alias xampp_stop="sudo /Applications/XAMPP/xamppfiles/xampp stop"
+alias xampp_start="sudo /Applications/XAMPP/xamppfiles/xampp start"
+alias xo='function __xo() { xampp && open "http://"$1; }; __xo'
