a100d124dc0622e70e1bb22c9fef02c1a3eac990
Micky Hulse <mhulse@concentricsky.com>
2019-10-23 16:57:17 -0700

    Alpha sort aliases
    #210

--- .bash/dev.bash
+++ .bash/dev.bash
@@ -1,7 +1,19 @@
-# Default Python:
-alias python=python3
+# List Atom packages:
+alias atompkgs="apm list --installed --bare"
+
+# Start blender from command line, used for debugging:
+alias blender="/Applications/blender.app/Contents/MacOS/blender"
+
+# Alias git:
+alias g="git"
+
+# Open current directory in Finder.app and VS Code:
+alias go="open . && code ."
+
 # Default pip:
 alias pip=pip3
+# Default Python:
+alias python=python3
 
 # Apache Tomcat start:
 # $ tomcat
@@ -10,20 +22,8 @@ alias pip=pip3
 # $ tomcat stop
 alias tomcat=catalina
 
-# List Atom packages:
-alias atompkgs="apm list --installed --bare"
-
-# Start blender from command line, used for debugging:
-alias blender="/Applications/blender.app/Contents/MacOS/blender"
-
-# Open current directory in Finder.app and Atom.app:
-alias go="open . && atom ."
-
 # XAMPP shortcuts:
 alias xampp="sudo /Applications/XAMPP/xamppfiles/xampp restart"
 alias xampp_stop="sudo /Applications/XAMPP/xamppfiles/xampp stop"
 alias xampp_start="sudo /Applications/XAMPP/xamppfiles/xampp start"
 alias xo='function __xo() { xampp && open "http://"$1; }; __xo'
-
-# Alias git:
-alias g="git"
