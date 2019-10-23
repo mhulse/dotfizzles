dee1df3a4d24773e54d0f2683f07316493c715cb
Micky Hulse <mhulse@concentricsky.com>
2019-10-23 16:38:21 -0700

    Make `g` alias for `git`
    Closes #207

--- .bash/dev.bash
+++ .bash/dev.bash
@@ -24,3 +24,6 @@ alias xampp="sudo /Applications/XAMPP/xamppfiles/xampp restart"
 alias xampp_stop="sudo /Applications/XAMPP/xamppfiles/xampp stop"
 alias xampp_start="sudo /Applications/XAMPP/xamppfiles/xampp start"
 alias xo='function __xo() { xampp && open "http://"$1; }; __xo'
+
+# Alias git:
+alias g="git"
